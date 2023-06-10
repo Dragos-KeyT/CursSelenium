7f9d6fc5e3e609fb16507a030a044f85d93ff437
Dragos Tanta <dragos.tanta@gmail.com>
2023-06-10 08:18:17 +0300

    curs7

--- src/main/java/tests/XpathExample.java
+++ src/main/java/tests/XpathExample.java
@@ -9,7 +9,7 @@ import utils.BaseTest;
 
 public class XpathExample extends BaseTest {
 
-	@Test
+	@Test(priority = 1)
 	public void xpathExampleTest() {
 		
 		//css --> li[class='menu_user_login']
@@ -28,9 +28,47 @@ public class XpathExample extends BaseTest {
 		WebElement passwordField = driver.findElement(By.xpath("//input[@id='password' and @name='pwd']"));
 		jse.executeScript("arguments[0].setAttribute('style', 'background:yellow; border:4px solid red')", passwordField);
 		passwordField.sendKeys("12345@67890");
+		//multiple elements
+		WebElement submitButton = driver.findElement(By.xpath("//ul//input[@class='submit_button']"));
+		jse.executeScript("arguments[0].setAttribute('style', 'background:yellow; border:4px solid red')", submitButton);
+		submitButton.click();
+		
 		
 	}
 	
+	@Test(priority=2)
+	public void xoathExample2() {
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+
+		driver.findElement(By.xpath("//span[@class='user_name']")).click();
+		
+	/*
+	* CONTAINS :
+	* <a href="https://keybooks.ro/wp-admin/profile.php" class="icon icon-cog">Settings</a>
+	* CSS :    a[class*='icon-cog']
+	* 
+	* XPATH : //a[contains(@class,'icon-cog')] 
+	*/	
+	driver.findElement(By.xpath("//a[contains(@class, 'icon-cog')]")).click();	
 	
+	//identificam pe baza textuli dintre tagurile HTML
+	// functia --> text()
+	driver.findElement(By.xpath("//a[text()= 'recent orders']")).click();
+	// direct child  si  contains(text() , valoare)
+	WebElement orderTableHeader = driver.findElement(By.xpath("//th[contains(@class, 'woocommerce-orders-table__header')]/span[contains(text(), 'Order')]"));
+	jse.executeScript("arguments[0].setAttribute('style', 'background:yellow; border:4px solid red')", orderTableHeader);
+	//index based
+	WebElement statusTableHeader = driver.findElement(By.xpath("(//th[contains(@class,'woocommerce-orders-table__header')]/span)[3]"));
+	jse.executeScript("arguments[0].setAttribute('style', 'background:yellow; border:4px solid red')", statusTableHeader);
+	//multiple attributes
+	WebElement order1721 = driver.findElement(By.xpath("//td[contains(@class, 'woocommerce-orders-table__cell')][@data-title='Order']/a[contains(text(), '1721')]"));
+	jse.executeScript("arguments[0].setAttribute('style', 'background:yellow; border:4px solid red')", order1721);
+	// not
+	WebElement order = driver.findElement(By.xpath("//td[contains(@class, 'woocommerce-orders-table__cell')][@data-title='Order']/a[not(contains(text(), '1721'))]"));
+	jse.executeScript("arguments[0].setAttribute('style', 'background:green; border:4px solid red')", order);
+
 	
+	}
+	
+	//starts-with
 }
